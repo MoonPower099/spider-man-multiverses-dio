a7a354b929cc494b7e38dc9cb9974ba80e5c7a68
Lucas Saramento Brandão <lucassbbrandao123@gmail.com>
2025-10-31 10:01:39 -0300

    Update index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,6 +8,53 @@ function handleMouseLeave() {
   document.body.id = '';
 }
 
+function getPreviousAndNextCard(currentSelectedCard) {
+  const MAX_CARDS = 3;
+  let previousCard = currentSelectedCard - 1;
+  let nextCard = currentSelectedCard + 1;
+
+  if (currentSelectedCard === MAX_CARDS) {
+    nextCard = 1;
+  }
+
+  if (currentSelectedCard === 1) {
+    previousCard = 3;
+  }
+
+  return {
+    previous: previousCard,
+    next: nextCard
+  }
+}
+
+function changeRotateZStyle(transform, rotateZDeg) {
+  return transform.replace(/rotateZ\((-?\d+deg)\)/i, `rotateZ(${rotateZDeg}deg)`);
+}
+
+function handleControllerButtonClick() {
+  const currentSelectedCard = Number(this.id);
+  const carousel = document.querySelector('.s-cards-carousel');
+  const transform = carousel.style.transform;
+  const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+  const rotateYDeg = -120 * (currentSelectedCard - 1);
+  const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+  carousel.style.transform = newTransform;
+
+  const activeButtonElement = document.querySelector('.s-controller__button--active');
+  activeButtonElement.classList.remove('s-controller__button--active');
+  this.classList.add('s-controller__button--active');
+
+  // Rotation cards effect
+
+  const cardsElements = document.getElementsByClassName('s-card');
+  const cards = getPreviousAndNextCard(currentSelectedCard);
+
+  cardsElements[cards.next - 1].style.transform = changeRotateZStyle(cardsElements[cards.next - 1].style.transform, -10);
+  cardsElements[cards.previous - 1].style.transform = changeRotateZStyle(cardsElements[cards.previous - 1].style.transform, 10);
+  cardsElements[currentSelectedCard - 1].style.transform = changeRotateZStyle(cardsElements[currentSelectedCard - 1].style.transform, 0);
+}
+
 function addEventListenersToCards() {
   const cardElements = document.getElementsByClassName('s-card');
   
@@ -18,19 +65,14 @@ function addEventListenersToCards() {
   }
 }
 
-document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
-
-function selectCarouselItem(selectedButtonElement) {
-  const selectedItem = selectedButtonElement.id;
-  const carousel = document.querySelector('.s-cards-carousel');
-  const transform = carousel.style.transform;
-  const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
-  const rotateYDeg = -120 * (Number(selectedItem) - 1);
-  const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
-
-  carousel.style.transform = newTransform;
-
-  const activeButtonElement = document.querySelector('.s-controller__button--active');
-  activeButtonElement.classList.remove('s-controller__button--active');
-  selectedButtonElement.classList.add('s-controller__button--active');
+function addEventListenersToCarouselButtons() {
+  const carouselButtons = document.getElementsByClassName('s-controller__button');
+  
+  for (let index = 0; index < carouselButtons.length; index++) {
+    const card = carouselButtons[index];
+    card.addEventListener('click', handleControllerButtonClick);
+  } 
 }
+
+document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
+document.addEventListener("DOMContentLoaded", addEventListenersToCarouselButtons, false);
